25a089f4d058c1c19b4124e36cbe0a9a4c483b17
PAIRI120 <zenako.id1@gmail.com>
2025-12-20 18:16:23 +0800

    Initial commit - membuat struktur awal

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>Test Aplikasi</title>
+</head>
+<body>
+    <h1>Selamat Datang di Aplikasi Desa!</h1>
+    <p>Jika Anda melihat ini, berarti lingkungan kerja Anda sudah siap.</p>
+</body>
+</html>
